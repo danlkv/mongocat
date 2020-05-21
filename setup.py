0d4749908d2b4955350f83e7500fe7c0b9026271
Danil Lykov <lkv97dn@gmail.com>
2020-05-21 16:59:31 -0500

    test change

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
+
     description="Cli tool to interact with mongodb",
     entry_points={
         'console_scripts': [
